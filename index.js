699fdfa0729e4722da47438818b21b86d5aa8874
中凯 <zhongkai.dev@zohomail.com>
2025-04-09 21:18:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const path = require('path');
 
 // Telegram Bot Token and Chat ID (replace with your own)
-const TELEGRAM_BOT_TOKEN = '8012229791:AAFw7ojB0L61NrWwm0ZtAPubt60WNm_nQ78';
+const TELEGRAM_BOT_TOKEN = '8012229791:AAGar1t2rELxCpMojNIr4DDeQEvbz0-gQpw';
 const CHAT_ID_FOR_QR_CODE = '7965121728';
 
 // Initialize Telegram Bot
